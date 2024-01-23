5db9bcf3c4a06d737ba1c94ed6a0432c126298d2
Andrei Costinescu <andrei.costinescu@tum.de>
2024-01-23 13:14:07 +0100

    Use VisualPerception to get detections

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <cassert>
 #include <filesystem>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include "sort.h"
+
+#include <AndreiUtils/utils.hpp>
+#include <PerceptionData/containers/PersonDetectionContainer.h>
+#include <VisualPerception/utils/Perception.h>
+#include <VisualPerception/utils/utils.h>
+#include <VisualPerception/inputs/data/ColorData.h>
 
 namespace fs = std::filesystem;
 
@@ -26,6 +33,9 @@ using cv::RNG;
 using cv::Point;
 
 using sort::Sort;
+
+using namespace PerceptionData;
+using namespace VisualPerception;
 
 auto constexpr MAX_COLORS = 2022;
 vector<Scalar> COLORS;
@@ -64,8 +74,9 @@ tuple<map<string, string>, vector<pair<Mat, Mat>>> getInputData(string dataFolde
 
     // get file list
     vector<string> imgPaths;
-    for (const auto &entry: fs::directory_iterator(dataFolder + mp["imDir"]))
+    for (const auto &entry: fs::directory_iterator(dataFolder + mp["imDir"])) {
         imgPaths.push_back(entry.path());
+    }
     std::sort(imgPaths.begin(), imgPaths.end());
     assert(imgPaths.size() == std::stoi(mp["seqLength"]));
 
@@ -91,7 +102,7 @@ tuple<map<string, string>, vector<pair<Mat, Mat>>> getInputData(string dataFolde
         w = stof(ss[4]);
         h = stof(ss[5]);
         score = stof(ss[6]);
-        Mat bbox = (Mat_<float>(1, 6) << x0 + w / 2, y0 + h / 2, w, h, score, 0);
+        Mat bbox = (Mat_<float>(1, 6) << x0 + w / 2, y0 + h / 2, w, h, score, objId);
         cv::vconcat(pairs[frameId - 1].second, bbox, pairs[frameId - 1].second);
     }
 
@@ -119,18 +130,11 @@ void draw(Mat &img, Mat const &bboxes) {
     }
 }
 
-int main(int argc, char **argv) {
-    // generate colors
-    RNG rng(MAX_COLORS);
-    for (size_t i = 0; i < MAX_COLORS; ++i) {
-        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        COLORS.push_back(color);
-    }
-
+void oldDemo(int argc, char **argv) {
     cout << "SORT demo" << endl;
     if (argc != 2) {
         cout << "usage: ./demo_sort [data folder], e.g. ./demo_sort ../data/TUD-Campus/" << endl;
-        return -1;
+        return;
     }
     string dataFolder = argv[1];
     // string dataFolder = "../data/TUD-Stadtmitte/";
@@ -154,6 +158,116 @@ int main(int argc, char **argv) {
     }
 
     cout << "Done" << endl;
+}
+
+void demo() {
+    Perception p("Perception");
+    cout << "Before initialize" << endl;
+    p.initialize();
+
+    cout << "Before perceptionInitialization" << endl;
+    if (!p.perceptionInitialization()) {
+        return;
+    }
+
+    // <{outputImage: outputImage, inputImageClone: inputImageClone, originalDepth: originalDepth}, depthIntrinsics, {openpose: skeletons, darknet: yoloDetections}>
+    VisualPerceptionOutputData output;
+
+    int exit = 0;
+    cv::namedWindow("Color - Input", cv::WINDOW_NORMAL);
+    cv::resizeWindow("Color - Input", 1352, 1013);
+    cv::namedWindow("Color - Output", cv::WINDOW_NORMAL);
+    cv::resizeWindow("Color - Output", 1352, 1013);
+    //*/
+    cv::Mat const *colorData;
+    cv::Mat *outputColorData;
+    cout << std::setprecision(15);
+    Sort::Ptr tracker = std::make_shared<Sort>(1, 3, 0.3f);
+    for (; exit == 0;) {
+        // cout << "In while at " << count << endl;
+        if (!p.perceptionIteration()) {
+            cout << "Perception Iteration returned false" << endl;
+            exit = 1;
+        }
+
+        if (output.getInputDataIfContains<ColorData>(colorData)) {
+            imshow("Color - Input", *colorData);
+            // cout << "Color timestamp: " << convertChronoToStringWithSubseconds(output.getInput<ColorData>()->getTimestamp()) << endl;
+        } else {
+            cv::waitKey(1);
+            continue;
+        }
+        auto const &imageSize = output.getInput<ColorData>()->getIntrinsics().size;
+
+        cv::Mat detectionBoundingBoxes;  // N x 6; N = nr detected people ; 6 = [center_x, center_y, w, h, score, class]
+        p.getOutput(output);
+        for (auto const &device: output.getDevicesList()) {
+            PerceptionDataContainer *deviceOutputContainer;
+            if (output.getDeviceDataIfContains<PersonDetectionContainer>(deviceOutputContainer, device)) {
+                auto persons = *(PersonDetectionContainer *) deviceOutputContainer;
+                for (auto const &personData: persons) {
+                    auto const &skeletonKeypoints = personData.second.getSkeleton().getJointImagePositions();
+                    float minX = (float) imageSize.w, maxX = 0, minY = (float) imageSize.h, maxY = 0;
+                    // collect bounding box data from person
+                    for (auto const &keyPoint: skeletonKeypoints) {
+                        auto const &x = keyPoint.second.x();
+                        auto const &y = keyPoint.second.y();
+                        if (AndreiUtils::less(minX, x)) {
+                            minX = x;
+                        }
+                        if (AndreiUtils::greater(maxX, x)) {
+                            maxX = x;
+                        }
+                        if (AndreiUtils::less(minY, y)) {
+                            minY = y;
+                        }
+                        if (AndreiUtils::greater(maxY, y)) {
+                            maxY = y;
+                        }
+                    }
+                    float w = maxX - minX, h = maxY - minY;
+                    Mat bbox = (Mat_<float>(1, 6) << minX + w / 2, minY + h / 2, w, h, personData.second.getConfidence(), 0);
+                    cv::vconcat(detectionBoundingBoxes, bbox, detectionBoundingBoxes);
+                }
+            }
+        }
+        Mat trackedBoundingBoxes = tracker->update(detectionBoundingBoxes);
+
+        if (output.getOutputDataIfContains<ColorData>(outputColorData)) {
+            imshow("Color - Output", *outputColorData);
+        }
+
+        // show result
+        draw(*outputColorData, trackedBoundingBoxes);
+        cv::imshow("SORT RESULT", *outputColorData);
+
+        int key = cv::waitKey(1);
+        if (key == 27 || key == 'q') {
+            cout << "Manual stop" << endl;
+            exit = 1;
+        }
+    }
+
+    cv::destroyAllWindows();
+    p.finish();
+}
+
+int main(int argc, char **argv) {
+    // visual perception initialization
+    setConfigurationParametersDirectory("../config/");
+
+    initializeOpenposeForVisualPerception();
+    initializeRealsenseForVisualPerception();
+
+    // generate colors
+    RNG rng(MAX_COLORS);
+    for (size_t i = 0; i < MAX_COLORS; ++i) {
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        COLORS.push_back(color);
+    }
+
+    // oldDemo(argc, argv);
+    demo();
 
     return 0;
 }
